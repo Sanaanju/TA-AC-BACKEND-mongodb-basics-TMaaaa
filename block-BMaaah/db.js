093da490a2710c5ba48c21a4716e00530809eb12
Sanaanju <sanaanjum1944@gmail.com>
2022-03-14 18:39:26 +0530

    Assignment2

---
+++ block-BMaaah/db.js
@@ -0,0 +1,36 @@
+Articles=[
+    {
+      title: 'Life',
+      details: 'Goals',
+      author: {
+        name: 'Sana',
+        email: 'sana123@.com',
+        age: '22',
+      },
+      tags: ['problems', 'achievements']
+    }
+    ,
+    {
+        
+      title: 'vegetables',
+      details: 'veggie',
+      author: {
+        name: 'potato',
+        email: 'potato123@.com',
+        age: '21',
+      },
+      tags: ['cook', 'eat']
+    }
+    ,
+    {
+           
+      title: 'learning',
+      details: 'code',
+      author: {
+        name: 'java',
+        email: 'java123@.com',
+        age: '67',
+      },
+      tags: ['compile', 'execute'],
+    },
+]
